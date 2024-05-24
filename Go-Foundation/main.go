8556c31baa1881bd65c67f31425dc0df882380f8
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-23 22:32:43 -0300

    Introducing to data types in GO

---
+++ Go-Foundation/main.go
@@ -0,0 +1,19 @@
+package main
+
+const a = "Pedro Cássio"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Pedro"
+	e float64 = 1.2
+)
+
+func main() {
+	println("Hello World! by: " + a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+
+}
